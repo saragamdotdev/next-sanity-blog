2e9177608bf711c5aa225324634cd39e1e4aa23c
saragam443 <git@saragam.com.np>
2023-11-18 15:51:18 +0545

    metadata

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -5,7 +5,7 @@ import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blog-Admin",
+  title: "Next-Sanity-Blog Admin",
   description: "Sanity Studio admin",
 };
 
